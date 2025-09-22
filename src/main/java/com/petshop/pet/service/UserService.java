4ae34b05707850494d2244ed30a78daae1b2557a
Chuong <vanchuong060704@gmail.com>
2025-09-22 13:39:06 +0700

    add UserService

--- src/main/java/com/petshop/pet/service/UserService.java
+++ src/main/java/com/petshop/pet/service/UserService.java
@@ -1,6 +1,49 @@
 package com.petshop.pet.service;
 
+import com.petshop.pet.domain.User;
+import com.petshop.pet.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(long id){
+        return userRepository.findById(id).get();
+    }
+
+    public User createUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+       return userRepository.save(user);
+    }
+
+    public User updateUser(long id, User userUpdate){
+        User user = userRepository.findById(id).get();
+        user.setFullName(userUpdate.getFullName());
+        user.setPhone(userUpdate.getPhone());
+        user.setAddress(userUpdate.getAddress());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(long id){
+        userRepository.deleteById(id);
+    }
 
 }
